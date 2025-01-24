1a0c19096e0a858d4433e74752e76ae36588d861
Barun <byadav23@gmail.com>
2025-01-24 16:24:09 -0500

    all changes

---
+++ src/main/java/com/example/spring_boot_ld/SpringBootLdApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_ld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLdApplication.class, args);
+	}
+
+}
